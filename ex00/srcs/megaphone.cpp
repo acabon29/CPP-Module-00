a87e43ed390ef57844a02abcd49c20eccf3287a3
Arthur Cabon <acabon@c1r2p12.42lehavre.fr>
2025-04-07 19:44:44 +0200

    initial commit

---
+++ ex00/srcs/megaphone.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// permmet de mettre automatiquement std dans std::count et autre
+// using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
